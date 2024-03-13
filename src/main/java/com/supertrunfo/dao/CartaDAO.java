b55dc787fee34b31ca20ab8f0c5d8975aed98983
Amanda Demétrio Alecrim <amanda2668@hotmail.com>
2024-03-13 17:14:12 -0300

    Fix conexão banco

--- src/main/java/com/supertrunfo/dao/CartaDAO.java
+++ src/main/java/com/supertrunfo/dao/CartaDAO.java
@@ -4,16 +4,25 @@ import com.supertrunfo.exceptions.ForcaExcedidaException;
 import com.supertrunfo.exceptions.NomeDuplicadoException;
 import com.supertrunfo.model.Carta;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CartaDAO {
-    private Connection conexao;
+    private static Connection conexao;
 
+    private static final String URL = "jdbc:postgresql://localhost:5432/super_trunfo";
+    private static final String USUARIO = "postgres";
+    private static final String SENHA = "postgres";
 
-    public void inserirCarta(Connection conexao, Carta carta) throws SQLException, ForcaExcedidaException, NomeDuplicadoException {
+    public static Connection obterConexao() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, SENHA);
+    }
+
+    public void inserirCarta(Carta carta) throws SQLException, ForcaExcedidaException, NomeDuplicadoException {
+
+        this.conexao = obterConexao();
+
         if (carta.getForca() + carta.getInteligencia() + carta.getVelocidade() > 15) {
             throw new ForcaExcedidaException("A soma de força, inteligência e velocidade não pode ser maior que 15.");
         }
@@ -33,6 +42,8 @@ public class CartaDAO {
     }
 
     private boolean existeCarta(String nome) throws SQLException {
+        this.conexao = obterConexao();
+
         String sql = "SELECT * FROM carta WHERE nome = ?";
         PreparedStatement ps = conexao.prepareStatement(sql);
         ps.setString(1, nome);
@@ -43,7 +54,9 @@ public class CartaDAO {
         return existe;
     }
 
-    public Carta buscarPorId(Connection conexao, int id) throws SQLException {
+    public Carta buscarPorId(int id) throws SQLException {
+        this.conexao = obterConexao();
+
         String sql = "SELECT * FROM carta WHERE id = ?";
         PreparedStatement ps = conexao.prepareStatement(sql);
         ps.setInt(1, id);
@@ -63,7 +76,9 @@ public class CartaDAO {
     }
 
 
-    public void atualizarCarta(Connection conexao, Carta carta) throws SQLException, ForcaExcedidaException {
+    public void atualizarCarta(Carta carta) throws SQLException, ForcaExcedidaException {
+        this.conexao = obterConexao();
+
         if (carta.getForca() + carta.getInteligencia() + carta.getVelocidade() > 15) {
             throw new ForcaExcedidaException("A soma de força, inteligência e velocidade não pode ser maior que 15.");
         }
@@ -79,12 +94,36 @@ public class CartaDAO {
         ps.close();
     }
 
-    public void excluirCarta(Connection conexao, int id) throws SQLException {
+    public void excluirCarta(int id) throws SQLException {
+        this.conexao = obterConexao();
+
         String sql = "DELETE FROM carta WHERE id = ?";
         PreparedStatement ps = conexao.prepareStatement(sql);
         ps.setInt(1, id);
         ps.executeUpdate();
         ps.close();
+    }
+
+    public List<Carta> obterTodasCartas() throws SQLException {
+        this.conexao = obterConexao();
+        List<Carta> todasCartas = new ArrayList<>();
+
+        String sql = "SELECT * FROM carta";
+        PreparedStatement ps = conexao.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            Carta carta = new Carta(
+                    rs.getInt("id"),
+                    rs.getString("nome"),
+                    rs.getInt("forca"),
+                    rs.getInt("inteligencia"),
+                    rs.getInt("velocidade")
+            );
+            todasCartas.add(carta);
+        }
+        rs.close();
+        ps.close();
+        return todasCartas;
     }
 
 }
