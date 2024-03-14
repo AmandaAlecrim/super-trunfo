76f49e4581a08e6646ecbabb2bc1404bb2bcdd6a
Amanda Demétrio Alecrim <amanda2668@hotmail.com>
2024-03-14 14:16:20 -0300

    ajustando captura de id

--- src/main/java/com/supertrunfo/dao/PartidaDAO.java
+++ src/main/java/com/supertrunfo/dao/PartidaDAO.java
@@ -20,7 +20,16 @@ public class PartidaDAO {
     public void inserirPartida(Partida partida) throws SQLException {
         this.conexao = obterConexao();
 
-        String sql = "INSERT INTO partida (rounds_vencidos_jogador, rounds_vencidos_cpu, rounds_empatados, resultado, data, forca_utilizada, inteligencia_utilizada, velocidade_utilidade) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        // Captura ID da partida
+        String sqlid = "SELECT nextval('partida_id_seq')";
+        PreparedStatement ps1 = conexao.prepareStatement(sqlid);
+        ResultSet rs = ps1.executeQuery();
+        if (rs.next()) {
+            int id = rs.getInt(1);
+            partida.setId(id);
+        }
+
+        String sql = "INSERT INTO partida (rounds_vencidos_jogador, rounds_vencidos_cpu, rounds_empatados, resultado, data, forca_utilizada, inteligencia_utilizada, velocidade_utilidade, id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
         PreparedStatement ps = conexao.prepareStatement(sql);
         ps.setInt(1, partida.getRoundsVencidosJogador());
         ps.setInt(2, partida.getRoundsVencidosCPU());
@@ -30,7 +39,8 @@ public class PartidaDAO {
         ps.setBoolean(6, partida.isForcaUtilizada());
         ps.setBoolean(7, partida.isInteligenciaUtilizada());
         ps.setBoolean(8, partida.isVelocidadeUtilizada());
-        ps.executeUpdate();
+        ps.setInt(9, partida.getId());
+        ps.execute();
         ps.close();
     }
 
@@ -62,17 +72,6 @@ public class PartidaDAO {
     public void atualizarPartida(Partida partida) throws SQLException {
         this.conexao = obterConexao();
 
-        // Consulta para obter o último ID inserido
-        String sqlUltimoId = "SELECT MAX(id) AS ultimo_id FROM partida";
-        Statement statement = conexao.createStatement();
-        ResultSet rs = statement.executeQuery(sqlUltimoId);
-        int ultimoId = 0;
-        if (rs.next()) {
-            ultimoId = rs.getInt("ultimo_id");
-        }
-        rs.close();
-        statement.close();
-
         String sql = "UPDATE partida SET rounds_vencidos_jogador = ?, rounds_vencidos_cpu = ?, rounds_empatados = ?, resultado = ?, data = ?, forca_utilizada = ?, inteligencia_utilizada = ?, velocidade_utilidade = ? WHERE id = ?";
         PreparedStatement ps = conexao.prepareStatement(sql);
         ps.setInt(1, partida.getRoundsVencidosJogador());
@@ -83,7 +82,7 @@ public class PartidaDAO {
         ps.setBoolean(6, partida.isForcaUtilizada());
         ps.setBoolean(7, partida.isInteligenciaUtilizada());
         ps.setBoolean(8, partida.isVelocidadeUtilizada());
-        ps.setInt(9, ultimoId);
+        ps.setInt(9, partida.getId());
         ps.executeUpdate();
         ps.close();
     }
